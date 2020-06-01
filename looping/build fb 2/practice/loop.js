93f8bda0bdc3ea4bb1a48bac2a71aa26c90f01b4
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-06-01 07:48:13 +0700

    practicing looping

---
+++ looping/build fb 2/practice/loop.js
@@ -0,0 +1,36 @@
+var database = [
+    {
+        username: "nopik",
+        password: "n0p1k"
+    }
+];
+
+var newsFeed = [
+    {
+        username: "Jono",
+        timeline: "Jono sedang makan"
+    },
+    {
+        username: "Dono",
+        timeline: "Dono sedang tidur"
+    },
+    {
+        username: "Dina",
+        timeline: "Dina sedang dandan"
+    }
+];
+
+var userNamePrompt = prompt("What's your username ?");
+var passWordPrompt = prompt ("What's your password ?");
+
+function signIn(user, pass) {
+    if(user === database[0].username && pass === database[0].password) {
+        alert(newsFeed);
+    } else {
+        alert("Maaf Anda belum terdaftar");
+
+    }
+}
+
+signIn();
+
