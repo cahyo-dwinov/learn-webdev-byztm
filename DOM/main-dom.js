70893881b5498b8d06e1a037fa80f79a5ce1b07e
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-06-03 21:39:02 +0700

    menyempurnakan fungsi kode

--- DOM/main-dom.js
+++ DOM/main-dom.js
@@ -2,20 +2,25 @@ var order = document.getElementById("order");
 var userinput = document.getElementById("userinput")
 var ul = document.querySelector("ul");
 
-order.addEventListener("click", function() {
-    if ( userinput.value.length > 0 ) {
+function userinputeLength() {
+    return userinput.value.length;
+}
+
+function createListElement() {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(userinput.value));
         ul.appendChild(li);
         userinput.value = "";
+}
+
+order.addEventListener("click", function() {
+    if ( userinputeLength() > 0 ) {
+        createListElement();
     }
 })
 
 userinput.addEventListener("keypress", function() {
-    if ( userinput.value.length > 0 && event.which === 13 ) {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(userinput.value));
-        ul.appendChild(li);
-        userinput.value = "";
+    if ( userinputeLength() > 0 && event.keyCode === 13 ) {
+        createListElement();
     }
 })
