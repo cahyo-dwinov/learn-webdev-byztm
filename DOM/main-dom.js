f5cb687e3c004337f9460cc7b289e646c2ac9b3d
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-06-03 21:43:41 +0700

    menyempurnakan kode

--- DOM/main-dom.js
+++ DOM/main-dom.js
@@ -6,6 +6,18 @@ function userinputeLength() {
     return userinput.value.length;
 }
 
+function addListAfterClick() {
+    if ( userinputeLength() > 0 ) {
+        createListElement();
+    }
+}
+
+function addListAfterKeypress(event) {
+    if ( userinputeLength() > 0 && event.keyCode === 13 ) {
+        createListElement();
+    }
+}
+
 function createListElement() {
         var li = document.createElement("li");
         li.appendChild(document.createTextNode(userinput.value));
@@ -13,14 +25,6 @@ function createListElement() {
         userinput.value = "";
 }
 
-order.addEventListener("click", function() {
-    if ( userinputeLength() > 0 ) {
-        createListElement();
-    }
-})
+order.addEventListener("click", addListAfterClick);
 
-userinput.addEventListener("keypress", function() {
-    if ( userinputeLength() > 0 && event.keyCode === 13 ) {
-        createListElement();
-    }
-})
+userinput.addEventListener("keypress", addListAfterKeypress);
