2a58b9491f63c4f88461bb992140ffe39de21ba2
Cahyo Dwinov <59456940+cahyo-dwinov@users.noreply.github.com>
2020-06-03 21:14:51 +0700

    mempraktikkan penambahan list baru dengan DOM

--- DOM/main-dom.js
+++ DOM/main-dom.js
@@ -1,5 +1,21 @@
-var button = document.querySelector("button");
+var order = document.getElementById("order");
+var userinput = document.getElementById("userinput")
+var ul = document.querySelector("ul");
 
-button.addEventListener("dblclick", function() {
-    console.log("Success");
+order.addEventListener("click", function() {
+    if ( userinput.value.length > 0 ) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(userinput.value));
+        ul.appendChild(li);
+        userinput.value = "";
+    }
+})
+
+userinput.addEventListener("keypress", function() {
+    if ( userinput.value.length > 0 && event.which === 13 ) {
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(userinput.value));
+        ul.appendChild(li);
+        userinput.value = "";
+    }
 })
